7faa108aadce13c1319bc8ca79de4c04962eb22e
akgom <132290469+akgom@users.noreply.github.com>
2026-03-24 14:14:58 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,29 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+roversaPet.setUltrasonicPins(DigitalPin.P13, DigitalPin.P14)
+roversaPet.setStopDistance(15)
+roversaPet.setCautionDistance(25)
+roversaPet.setPetStats(70, 20, 80)
+
+input.onButtonPressed(Button.A, function () {
+    roversaPet.petRobot()
+})
+
+input.onButtonPressed(Button.B, function () {
+    roversaPet.feedRobot()
+})
+
+input.onButtonPressed(Button.AB, function () {
+    roversaPet.showDistance()
+})
+
+basic.forever(function () {
+    roversaPet.updatePetState()
+    roversaPet.updateFace()
+
+    if (roversaPet.dangerDetected()) {
+        Roversa.stop()
+    } else {
+        Roversa.driveForward()
+    }
+
+    basic.pause(500)
+})
